d56779189bfa4915954a69c2eb659f456b154528
BorisNgd <bngd228@gmail.com>
2020-04-10 07:19:10 +0100

    ok

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const server = require('./configs/app')();
-const config = require('./configs/config/config');
+//const config = require('./configs/config/config');
 const db = require('./configs/db');
+const functions = require('firebase-functions');
+    
 
 //create the basic server setup
 server.create(db);
 
 //start the server
 server.start();
+
+server.get('/time' , (req , res , next) =>{
+    res.send('Okay');
+})
+
+exports.server = functions.https.onRequest(server); 
