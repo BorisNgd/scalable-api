609e027f9742513c0b7f3218947cb443e6d3bde1
Boris Ngd Y <34988765+BorisNgd@users.noreply.github.com>
2020-05-05 15:57:28 +0100

    Update index.js

--- routes/public/index.js
+++ routes/public/index.js
@@ -2,6 +2,5 @@
 const express = require('express');
 let router = express.Router();
 router.get('/' , (req , res , next) =>{
-  res.send('<script data-ad-client="ca-pub-5835462636454179" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
-});
-module.exports = router;<script data-ad-client="ca-pub-5835462636454179" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  res.send('Hello world'});
+module.exports = router;
